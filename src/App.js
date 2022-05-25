d7a944dfe72dc4fcf0bc157ce8b271f57f6bbe52
Suraj Magdum <magdumsuraj07@gmail.com>
2022-05-25 22:32:16 +0530

    Added register form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import "./App.css";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <main className="container">
         <Routes>
           <Route path="login" element={<LoginForm />} />
+          <Route path="register" element={<RegisterForm />} />
           <Route path="movies/:id" element={<MovieForm />} />
           <Route path="movies" element={<Movies />} />
           <Route path="customers" element={<Customers />} />
